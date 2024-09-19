32882504963852963971d8d80c25105971e6b26a
Ali Youssef <aymost@utu.fi>
2024-09-19 23:02:02 +0300

    .

--- tomakeapackae.r
+++ tomakeapackae.r
@@ -13,7 +13,10 @@ Rcpp::compileAttributes()
 library(devtools)
 #test()
 
-devtools::check()
+devtools::check(vignettes = FALSE)
+
+devtools::check(vignettes = TRUE)
+
 devtools::build(vignettes = FALSE)
 devtools::build_vignettes()
 
