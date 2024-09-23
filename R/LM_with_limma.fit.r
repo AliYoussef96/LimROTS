2664bb2a9b7b534d5356b25fea90bea62f17688a
AliYoussef96 <aliali.mostafa99@gmail.com>
2024-09-23 14:55:58 +0300

    .

--- R/LM_with_limma.fit.r
+++ R/LM_with_limma.fit.r
@@ -45,58 +45,61 @@
 #'
 
 
+
 testStatistic_with_covariates_Fit <- function(data,
                                               group.name,
                                               meta.info ,
                                               formula.str,
                                               trend,
                                               robust) {
-  combined_data <- data.frame(
-    check.rows = FALSE,
-    check.names = FALSE ,
-    none = rep("none" , nrow(data[[1]]))
-  )
-  for (k.list in names(data)) {
-    combined_data <- cbind(combined_data,
-                           data.frame(data[[k.list]], check.rows = FALSE, check.names = FALSE))
-  }
-  combined_data <- combined_data[, -1]
-  design.matrix <- model.matrix(formula(formula.str), data = meta.info)
-  colnames(design.matrix) <- make.names(colnames(design.matrix))
-  fit <- limma::lmFit(combined_data, design.matrix)
-  if (length(data) == 2) {
-    pairwise_contrasts <- paste0(group.name , unique(meta.info[, group.name]))
-    pairwise_contrasts <- combn(pairwise_contrasts, 2, function(x)
-      paste(x[1], "-", x[2]))
-    cont_matrix <- limma::makeContrasts(contrasts = pairwise_contrasts, levels =
-                                          design.matrix)
-    fit2 <- limma::contrasts.fit(fit, cont_matrix)
-    fit.ebayes <- limma::eBayes(fit2, trend = trend, robust = robust)
-    d_values <- limma::topTable(fit.ebayes,
-                                coef = pairwise_contrasts ,
-                                number = "Inf",
-                                sort.by = 'none')
-    corrected.logfc <- d_values$logFC
-    d_values <- abs(d_values$logFC)
-    s_values <- as.numeric(sqrt(fit.ebayes$s2.post)  * fit.ebayes$stdev.unscaled[, 1])
-    return(list(
-      d = d_values,
-      s = s_values ,
-      corrected.logfc = corrected.logfc
-    ))
-  } else if (length(data) > 2 & ncol(meta.info) == 1) {
-    pairwise_contrasts <- paste0(group.name , unique(meta.info[, group.name]))
-    pairwise_contrasts <- combn(pairwise_contrasts, 2, function(x)
-      paste(x[1], "-", x[2]))
-    cont_matrix <- limma::makeContrasts(contrasts = pairwise_contrasts, levels = design.matrix)
-    fit2 <- limma::contrasts.fit(fit, cont_matrix)
-    fit.ebayes <- limma::eBayes(fit2, trend = trend, robust = robust)
-    msr <- fit.ebayes$F * fit.ebayes$s2.post
-    corrected.logfc <- fit.ebayes$coefficients
-    return(list(
-      d = msr,
-      s = fit.ebayes$s2.post ,
-      corrected.logfc = corrected.logfc
-    ))
-  }
+    combined_data <- data.frame(
+        check.rows = FALSE,
+        check.names = FALSE ,
+        none = rep("none" , nrow(data[[1]]))
+    )
+    for (k.list in names(data)) {
+        combined_data <- cbind(combined_data,
+                               data.frame(data[[k.list]], check.rows = FALSE, check.names = FALSE))
+    }
+    combined_data <- combined_data[, -1]
+    design.matrix <- model.matrix(formula(formula.str), data = meta.info)
+    colnames(design.matrix) <- make.names(colnames(design.matrix))
+    fit <- limma::lmFit(combined_data, design.matrix)
+    if (length(data) == 2) {
+        pairwise_contrasts <- paste0(group.name , unique(meta.info[, group.name]))
+        pairwise_contrasts <- combn(pairwise_contrasts, 2, function(x)
+            paste(x[1], "-", x[2]))
+        cont_matrix <- limma::makeContrasts(contrasts = pairwise_contrasts, levels =
+                                                design.matrix)
+        fit2 <- limma::contrasts.fit(fit, cont_matrix)
+        fit.ebayes <- limma::eBayes(fit2, trend = trend, robust = robust)
+        d_values <- limma::topTable(
+            fit.ebayes,
+            coef = pairwise_contrasts ,
+            number = "Inf",
+            sort.by = 'none'
+        )
+        corrected.logfc <- d_values$logFC
+        d_values <- abs(d_values$logFC)
+        s_values <- as.numeric(sqrt(fit.ebayes$s2.post)  * fit.ebayes$stdev.unscaled[, 1])
+        return(list(
+            d = d_values,
+            s = s_values ,
+            corrected.logfc = corrected.logfc
+        ))
+    } else if (length(data) > 2 & ncol(meta.info) == 1) {
+        pairwise_contrasts <- paste0(group.name , unique(meta.info[, group.name]))
+        pairwise_contrasts <- combn(pairwise_contrasts, 2, function(x)
+            paste(x[1], "-", x[2]))
+        cont_matrix <- limma::makeContrasts(contrasts = pairwise_contrasts, levels = design.matrix)
+        fit2 <- limma::contrasts.fit(fit, cont_matrix)
+        fit.ebayes <- limma::eBayes(fit2, trend = trend, robust = robust)
+        msr <- fit.ebayes$F * fit.ebayes$s2.post
+        corrected.logfc <- fit.ebayes$coefficients
+        return(list(
+            d = msr,
+            s = fit.ebayes$s2.post ,
+            corrected.logfc = corrected.logfc
+        ))
+    }
 }
