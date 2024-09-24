a46bdfbafdc3ac28e511b955c8c4963a54607e20
AliYoussef96 <aliali.mostafa99@gmail.com>
2024-09-24 15:31:29 +0300

    columbia dataset

---
+++ R/Columbia.RealCase.r
@@ -0,0 +1,10 @@
+#' Columbia Dataset Realcase
+#'
+#' A \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}} object containing DIA proteomics data from a UPS1-spiked E. coli protein mixture, processed using Spectronaut.
+#'
+#' @name Columbia.RealCase
+#' @docType data
+#' @format An instance of the \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}} class with the following assays:
+#'
+
+NULL
