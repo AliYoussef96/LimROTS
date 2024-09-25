5702b1043de09c206f5e779fff9e7198af81f169
AliYoussef96 <aliali.mostafa99@gmail.com>
2024-09-25 12:50:07 +0300

    .

---
+++ R/proteogenomicPD.r
@@ -0,0 +1,10 @@
+#' proteogenomicPD Dataset
+#'
+#' A \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}} object
+#'
+#' @name proteogenomicPD
+#' @docType data
+#' @format An instance of the \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}} class with the following assays:
+#'
+
+NULL
