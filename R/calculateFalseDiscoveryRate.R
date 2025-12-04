10aee7033984b41dc5b0350389663a2ac705455d
Ali Youssef <aliali.mostafa99@gmail.com>
2025-12-04 19:56:18 +0200

    Add Copyright statement to calculateFalseDiscoveryRate.R

--- R/calculateFalseDiscoveryRate.R
+++ R/calculateFalseDiscoveryRate.R
@@ -1,3 +1,16 @@
+# /*
+#  * Adapted from ROTS Bioconductor (3.19, 2024) files calculateFDR.R
+#  * and biggerN.R
+#  * Original authors: Suomi T, Seyednasrollah F, Jaakkola MK, Faux T, Elo LL
+#  * (2017) "ROTS: An R package for reproducibility-optimized statistical
+#  * testing." PLOS Computational Biology 13(5): e1005562.
+#  * DOI: https://doi.org/10.1371/journal.pcbi.1005562
+#  * Copyright (C) 2025 Ali Youssef et al.
+#  * Modifications: The method for calculating the FDR was adapted and the
+#  * function was refactored, while the `countLargerThan` function remains
+#  * unchanged compared to `ROTS::biggerN`.
+#  */
+#
 #' Calculate False Discovery Rate (FDR) Using Permuted Values (Adjusted)
 #'
 #' This function calculates the false discovery rate (FDR) by comparing
