7a7d9030efecbca6de44ba14e40355e32c542c93
Ali Youssef <aliali.mostafa99@gmail.com>
2025-12-05 00:28:16 +0200

    Modifying copyright statement in Bootstrap_functions.r

--- R/Bootstrap_functions.r
+++ R/Bootstrap_functions.r
@@ -1,10 +1,8 @@
 # /*
 #  * Adapted from ROTS Bioconductor (3.19, 2024) files bootstrapSamples.R
-#  * Original authors: Suomi T, Seyednasrollah F, Jaakkola MK, Faux T, Elo LL
-#  * (2017) "ROTS: An R package for reproducibility-optimized statistical
-#  * testing." PLOS Computational Biology 13(5): e1005562.
-#  * DOI: https://doi.org/10.1371/journal.pcbi.1005562
-#  * Copyright (C) 2025 Ali Youssef et al.
+#  * Copyright on original version by: F. Seyednasrollah, T. Suomi,
+#  * L.L. Elo (2024)
+#  * Copyright on modifications (C) 2024-2025 A. Youssef
 #  * Modifications: The `bootstrapS` implementation was not changed
 #  * functionally but was refactored, and `bootstrapSamples_limRots`
 #  * is a new implementation supporting the LimROTS approach for
